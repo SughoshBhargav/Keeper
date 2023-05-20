c8b1a40325d7680946a346e4d72311e5dc545399
SughoshBhargav <sughoshsv@.com>
2023-05-20 10:50:09 +0530

    Updates

---
+++ precache-manifest.2af278195cb178d7e45769adace12ec6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7164ebee8006fe2dcbd2516f169d638b",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "b290ee9f4cb461713ff6",
+    "url": "/Keeper/static/js/2.20e9fc77.chunk.js"
+  },
+  {
+    "revision": "8eb49c61de3e063a81e6",
+    "url": "/Keeper/static/js/main.fb4573b5.chunk.js"
+  },
+  {
+    "revision": "573da01c7c909d69a3f3",
+    "url": "/Keeper/static/js/runtime-main.13d65c7f.js"
+  }
+]);
